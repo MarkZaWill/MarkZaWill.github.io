bb2ff711c26be2f87c3127b39b38567217daaed3
Mark Zachary <MarkZaWill@gmail.com>
2016-07-05 17:18:59 -0500

    initial commit

---
+++ app/app.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/* exported PersonalSite */
+
+let PersonalSite = angular.module("PersonalSite", ["ngRoute"]);
+
+
+
+
+
+/*
+  Set up routes for the app
+ */
+PersonalSite.config(["$routeProvider",
+  function ($routeProvider) {
+    $routeProvider.
+
+      when("/", {
+        templateUrl: "partials/Welcome.html",
+         // controller: "LoginCtrl"
+      }).
+      when("/psych", {
+        templateUrl: "partials/Psychology.html",
+        // controller: "LoginCtrl"
+      }).
+      when("/dev", {
+        templateUrl: "partials/Developer.html",
+        // controller: "LoginCtrl"
+      }).
+      when("/about", {
+        templateUrl: "partials/AboutMe.html",
+        // controller: "LoginCtrl"
+      }).  
+      otherwise({
+        redirectTo: "/"
+      });
+  }]);
