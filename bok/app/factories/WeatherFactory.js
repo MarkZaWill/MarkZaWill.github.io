e0457d3054ca813bfb4fe87bb77b907572ac391c
Mark Zachary <MarkZaWill@gmail.com>
2016-07-06 13:21:09 -0500

    broke my weather api

--- bok/app/factories/WeatherFactory.js
+++ bok/app/factories/WeatherFactory.js
@@ -12,7 +12,7 @@ $(function() {
 
   
   $.ajax({
-    url: 'https://api.openweathermap.org/data/2.5/weather?zip=37210,us&appid=d3bd75156e1363b7b3c8e43c98744398',
+    url: 'http://api.openweathermap.org/data/2.5/weather?zip=37210,us&appid=d3bd75156e1363b7b3c8e43c98744398',
     dataType: 'jsonp',
     success: function(results) {
       var currentWeather = results.weather[0].id;
